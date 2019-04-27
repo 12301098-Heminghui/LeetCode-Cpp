0f7c37c44eb09ffae9d20686b33787c545e0db16
heminghui_i <heminghui_i@didichuxing.com>
2019-04-27 23:39:24 +0800

    update

---
+++ 28.strStr.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle == "" or (haystack == needle)){
+            return 0;
+        }
+        int m = haystack.size();
+        int n = needle.size();
+        for(int i = 0; i < m-n+1; i++){
+            int j = 0;
+            for(;j < n; j++){
+                if(haystack[i+j] != needle[j]){
+                    break;
+                }
+            }
+            if(j == n){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+};
